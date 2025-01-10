8c6f85dd909beab376713248bfdf8293cbfb3000
sallu-786 <166565@toyota-boshoku.co.jp>
2025-01-10 16:14:22 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,4 @@ app = FastAPI()
 def read_main():
     return {"message": "Hello World from main app"}
 
-mount_chainlit(app=app, target="cl_app2.py", path="/codepilot")
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000) 
+mount_chainlit(app=app, target="cl_app.py", path="/codepilot")
